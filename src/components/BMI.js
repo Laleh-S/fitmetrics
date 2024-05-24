1e5e3707e1dadf6bf0b0927fb2ef27f85adf607a
Laleh Shahidi <laleh.shahidi78@gmail.com>
2024-05-24 16:37:48 +0100

    removed TypeScript

---
+++ src/components/BMI.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+function BMI () {
+
+    return (
+        <div>
+            <h1>Welcome to FitMetrics</h1>
+        </div>
+    );
+};
+
+export default BMI;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,7 @@ function Navbar () {
     return (
         <nav className="bg-sky-950 border h-24">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-            <Link to="/" className="text-white text-xl font-bold">FitMetrics</Link> 
+            <Link to="/" className="text-white text-xl font-bold">FitMatrics</Link> 
             </div>
         </nav>
     );
