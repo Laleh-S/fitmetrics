e8d3cefed8f01d3282b59fb2d069e74421a408a5
Laleh Shahidi <laleh.shahidi78@gmail.com>
2024-05-28 09:18:04 +0100

    created states for weight, height and bmi

--- src/context/CalculatorContext.js
+++ src/context/CalculatorContext.js
@@ -1,9 +1,32 @@
-import { React, createContext } from "react";
+import { React, createContext,useState } from "react";
 
 const CalculatorContext = createContext();
 
 function CalculatorContextProvider({ children }) {
-  const contextValue = {};
+   // "" act as placeholder for form input. Stating the user hasn't input any value yet, when component first renders.
+  // null indicates that the BMI value is not yet calculated or available.
+  const [weight, setWeight] = useState(""); 
+  const [height, setHeight] = useState("");
+  const [bmi, setBmi] = useState(null);
+
+
+  // Calculating BMI function
+  const calculateBMI = () => {
+    const heightToMeter = height / 100
+    const bmiValue = weight / (heightToMeter * heightToMeter)
+    setBmi(bmiValue)
+};
+
+
+  const contextValue = {
+    weight, 
+    setWeight,
+    height, 
+    setHeight,
+    bmi, 
+    setBmi,
+    calculateBMI,
+  };
 
   return (
     <CalculatorContext.Provider value={contextValue}>
