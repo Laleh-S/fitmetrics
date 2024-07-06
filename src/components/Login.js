51dede058b35b07803e22a6329d742280a1831dc
Laleh Shahidi <laleh.shahidi78@gmail.com>
2024-07-06 11:54:05 +0200

    added some styling to login

--- src/components/Login.js
+++ src/components/Login.js
@@ -33,39 +33,45 @@ const Login = () => {
     };
     
     return (
-        <div className="">
-            <form className="" onSubmit={handleSubmit}>
-                <h2>Login</h2>
-                {loginSuccess && <p style={{ color: 'green' }}>Login successful!</p>}
-                {error && <p style={{ color: 'red' }}>{error}</p>}
-                <label className="">
-                    Email
-                    <input
-                        type="text"
-                        placeholder="Email" 
-                        value={email} 
-                        onChange={handdleEmailChange}
-                        className=""
-                        required
-                    />
-                </label>
-                <label className="">
-                    Password
-                    <input
-                        type="text"
-                        placeholder="Password" 
-                        value={password} 
-                        onChange={handdlePasswordChange}
-                        className=""
-                        required
-                    />
-                </label>
-                <button type="submit" className="">Login</button>
-                <div>
-                    Don't have an account? 
-                    <Link to="/register" className="text-blue-500 font-bold"> Register now</Link>
-                </div>
-            </form>
+        <div className="flex flex-wrap items-center mt-12 px-4">
+            <div className="w-full md:w-1/2 max-w-lg mx-auto mt-8 p-6 border border-gray-200 rounded-lg shadow-md bg-grey">
+                <form className="" onSubmit={handleSubmit}>
+                    <h2 className="text-xl font-semibold mb-4">Login into your account</h2>
+                    {loginSuccess && <p style={{ color: 'green' }}>Login successful!</p>}
+                    {error && <p style={{ color: 'red' }}>{error}</p>}
+                    <div className="mb-4">
+                        <label className="">
+                            Email
+                            <input
+                                type="text"
+                                placeholder="Email" 
+                                value={email} 
+                                onChange={handdleEmailChange}
+                                className="mt-1 block w-full p-2 border border-gray-300 rounded-md"
+                                required
+                            />
+                        </label>
+                    </div>
+                    <div  className="mb-4">
+                        <label className="mb-4">
+                            Password
+                            <input
+                                type="password"
+                                placeholder="Password" 
+                                value={password} 
+                                onChange={handdlePasswordChange}
+                                className="mt-1 block w-full p-2 border border-gray-300 rounded-md"
+                                required
+                            />
+                        </label>
+                    </div>
+                    <button type="submit" className="w-full bg-primary text-white py-2 px-4 rounded-md hover:bg-blue-800 transition duration-300">Login</button>
+                    <div className="mt-5 text-center ">
+                        Don't have an account? 
+                        <Link to="/register" className="text-blue-500 font-bold"> Register here</Link>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 };
